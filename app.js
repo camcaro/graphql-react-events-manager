6e21f85b21ae21ed148a54ba48f93346f233607a
camcaro <carlos.camara.ortiz@gmail.com>
2021-11-05 12:27:19 +0100

    add graphql basic schema and resolvers + graphiql test interface working

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const exp = require('constants');
 const express = require('express');
 //const bodyParser = require('body-parser'); now deprecated
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 //app.use(bodyParser.json()); express now able to parse json
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
-app.listen(3000);
+        type RootMutation {
+            createEvent(name: String!): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Coding this app', 'Sailing', 'Studying for that interview'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3000); 
 
