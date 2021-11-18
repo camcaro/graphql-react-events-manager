5d7b7aa317692ba8ab9502267ec583c170c432ba
camcaro <carlos.camara.ortiz@gmail.com>
2021-11-18 16:01:48 +0100

    refactor(graphql): resolvers and schema to their own files

--- app.js
+++ app.js
@@ -1,163 +1,18 @@
 const exp = require('constants');
-//const e = require('express');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event.js'); 
-const User = require('./models/user.js');
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
 
 const app = express();
 
 app.use(express.json());
 
-const events = eventIds => {
-    return Event.find({ _id: { $in: eventIds}})
-        .then(events => {
-            return events.map(event => {
-                return { 
-                    ...event._doc,
-                    creator: user.bind(this, event._doc.creator)
-                };
-            });
-        })
-        .catch(err => {
-            throw err;
-        })
-};
-
-const user = userId => { // used to unpack (populate) the user object when referenced
-    return User.findById(userId)
-        .then(user => {
-            return { 
-                ...user._doc,
-                createdEvents: events.bind(this, user._doc.createdEvents)
-            };
-        })
-        .catch(err => {
-            throw err;
-        })
-};
-
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-            creator: User!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-            createdEvents: [Event!]
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find()
-                    .then(events => {
-                        return events.map(event => {
-                            //console.log("event id: " + event._doc._id.toString()); // No need to parse like this at below return
-                            return { 
-                                ...event._doc,
-                                creator: user.bind(this, event._doc.creator)
-                            };
-                        });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        throw(err);
-                    });
-        },
-        createEvent: args => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price, //+ before args converts to number in case it was not
-                date: new Date(args.eventInput.date),
-                creator: '618ade5925183b056c5af594'
-            });
-            let createdEvent;
-            return event
-                .save()
-                .then(result => {
-                    createdEvent = { ...result._doc, creator: user.bind(this, result._doc.creator) };
-                    return User.findById('618ade5925183b056c5af594')
-                })
-                .then(user => {
-                    if (!user) {
-                        throw new Error('User not found');
-                    }
-                    user.createdEvents.push(event); // can add just the document id or the whole object
-                    return user.save();
-                })
-                .then(result => {
-                    return createdEvent;                    
-                })
-                .catch(err => {
-                    console.log(err);
-                    throw err;
-                }); 
-        },
-        createUser: args => {
-            return User.findOne({ email: args.userInput.email }).then(user => {
-             if (user) {
-                 throw new Error('User exists already');
-             }
-             return bcrypt
-                 .hash(args.userInput.password, 12)
-            })
-            .then(hashedPassword => {
-                const user = new User({
-                    email: args.userInput.email,
-                    password: hashedPassword // ***DO NOT store as plaintext!***
-                });
-                return user.save()
-                    .then(result => {
-                        return { ...result._doc, password: null };
-                    })
-                    .catch(err => {
-                        throw err;
-                    });
-                })
-                .catch(err => {
-                    throw err;
-                });
-        }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
 }));
 
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,108 @@
+const bcrypt = require('bcryptjs');
+
+const Event = require('../../models/event.js'); 
+const User = require('../../models/user.js');
+
+
+const events = eventIds => {
+    return Event.find({ _id: { $in: eventIds}})
+        .then(events => {
+            return events.map(event => {
+                return { 
+                    ...event._doc,
+                    creator: user.bind(this, event._doc.creator)
+                };
+            });
+        })
+        .catch(err => {
+            throw err;
+        })
+};
+
+const user = userId => { // used to unpack (populate) the user object when referenced
+    return User.findById(userId)
+        .then(user => {
+            return { 
+                ...user._doc,
+                createdEvents: events.bind(this, user._doc.createdEvents)
+            };
+        })
+        .catch(err => {
+            throw err;
+        })
+};
+
+
+module.exports = {
+    events: () => {
+        return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        //console.log("event id: " + event._doc._id.toString()); // No need to parse like this at below return
+                        return { 
+                            ...event._doc,
+                            creator: user.bind(this, event._doc.creator)
+                        };
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw(err);
+                });
+    },
+    createEvent: args => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price, //+ before args converts to number in case it was not
+            date: new Date(args.eventInput.date),
+            creator: '618ade5925183b056c5af594'
+        });
+        let createdEvent;
+        return event
+            .save()
+            .then(result => {
+                createdEvent = { ...result._doc, creator: user.bind(this, result._doc.creator) };
+                return User.findById('618ade5925183b056c5af594')
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error('User not found');
+                }
+                user.createdEvents.push(event); // can add just the document id or the whole object
+                return user.save();
+            })
+            .then(result => {
+                return createdEvent;                    
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            }); 
+    },
+    createUser: args => {
+        return User.findOne({ email: args.userInput.email }).then(user => {
+         if (user) {
+             throw new Error('User exists already');
+         }
+         return bcrypt
+             .hash(args.userInput.password, 12)
+        })
+        .then(hashedPassword => {
+            const user = new User({
+                email: args.userInput.email,
+                password: hashedPassword // ***DO NOT store as plaintext!***
+            });
+            return user.save()
+                .then(result => {
+                    return { ...result._doc, password: null };
+                })
+                .catch(err => {
+                    throw err;
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
+    }
+};
