58a002715aba84c4a8b2cc5b11a3329faca6f932
camcaro <carlos.camara.ortiz@gmail.com>
2021-11-19 13:01:14 +0100

    style: add initial auto reformat with prettier

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const exp = require('constants');
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const mongoose = require('mongoose');
+const exp = require("constants");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 
-const graphQlSchema = require('./graphql/schema');
-const graphQlResolvers = require('./graphql/resolvers');
+const graphQlSchema = require("./graphql/schema");
+const graphQlResolvers = require("./graphql/resolvers");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/graphql', graphqlHTTP({
-    schema: graphQlSchema,
-    rootValue: graphQlResolvers,
-    graphiql: true
-}));
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: graphQlSchema,
+		rootValue: graphQlResolvers,
+		graphiql: true
+	})
+);
 
 // Replaced mongo_db_admin:<password> with env variables + database name
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
-}@cluster0.mvjis.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-).then(() => {
-    app.listen(3000);  
-}).catch(err => {
-    console.log(err);
-}); 
-
-
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.mvjis.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		app.listen(3000);
+	})
+	.catch(err => {
+		console.log(err);
+	});
