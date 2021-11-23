8dade327502b1c1de7afff06fcb89b1adc4bc904
camcaro <carlos.camara.ortiz@gmail.com>
2021-11-23 11:20:22 +0100

    feat: add bookings referenced to user and event

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,9 +2,10 @@ const bcrypt = require("bcryptjs");
 
 const Event = require("../../models/event.js");
 const User = require("../../models/user.js");
+const Booking = require("../../models/booking.js");
 
-const convertDate = obj => {
-	return new Date(obj._doc.date).toISOString();
+const convertDate = date => {
+	return new Date(date).toISOString();
 };
 
 const events = async eventIds => {
@@ -13,10 +14,23 @@ const events = async eventIds => {
 		return eventsReceived.map(event => {
 			return {
 				...event._doc,
-				date: convertDate(event),
+				date: convertDate(event._doc.date),
 				creator: user.bind(this, event._doc.creator)
 			};
 		});
+	} catch (err) {
+		throw err;
+	}
+};
+
+const singleEvent = async eventId => {
+	try {
+		const receivedEvent = await Event.findById(eventId);
+		return {
+			...receivedEvent._doc,
+			date: convertDate(receivedEvent._doc.date),
+			creator: user.bind(this, receivedEvent._doc.creator)
+		};
 	} catch (err) {
 		throw err;
 	}
@@ -43,12 +57,29 @@ module.exports = {
 				//console.log("event id: " + event._doc._id.toString()); // No need to parse like this at below return
 				return {
 					...event._doc,
-					date: convertDate(event),
+					date: convertDate(event._doc.date),
 					creator: user.bind(this, event._doc.creator)
 				};
 			});
 		} catch (err) {
 			console.log(err);
+			throw err;
+		}
+	},
+	bookings: async () => {
+		try {
+			const bookings = await Booking.find();
+			// console.log({ ...bookings[0]._doc._id });
+			return bookings.map(booking => {
+				return {
+					...booking._doc,
+					event: singleEvent.bind(this, booking._doc.event),
+					user: user.bind(this, booking._doc.user),
+					createdAt: convertDate(booking._doc.createdAt),
+					updatedAt: convertDate(booking._doc.updatedAt)
+				};
+			});
+		} catch (err) {
 			throw err;
 		}
 	},
@@ -58,17 +89,17 @@ module.exports = {
 			description: args.eventInput.description,
 			price: +args.eventInput.price, //+ before args converts to number in case it was not
 			date: new Date(args.eventInput.date),
-			creator: "618ade5925183b056c5af594"
+			creator: "6197d4453a971fbf18a09a4f"
 		});
 		let createdEvent;
 		try {
 			const result = await event.save();
 			createdEvent = {
 				...result._doc,
-				date: convertDate(result),
+				date: convertDate(result._doc.date),
 				creator: user.bind(this, result._doc.creator)
 			};
-			const creator = await User.findById("618ade5925183b056c5af594");
+			const creator = await User.findById("6197d4453a971fbf18a09a4f");
 			if (!creator) {
 				throw new Error("User not found");
 			}
@@ -95,6 +126,25 @@ module.exports = {
 			return {
 				...result._doc,
 				password: null
+			};
+		} catch (err) {
+			throw err;
+		}
+	},
+	bookEvent: async args => {
+		try {
+			const fetchedEvent = await Event.findOne({ _id: args.eventId });
+			const booking = new Booking({
+				user: "6197d4453a971fbf18a09a4f",
+				event: fetchedEvent
+			});
+			const result = await booking.save();
+			return {
+				...result._doc,
+				event: singleEvent.bind(this, result._doc.event),
+				user: user.bind(this, result._doc.user),
+				createdAt: convertDate(result._doc.createdAt),
+				updatedAt: convertDate(result._doc.updatedAt)
 			};
 		} catch (err) {
 			throw err;
